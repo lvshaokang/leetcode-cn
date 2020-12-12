cd84e2e977f1b5812e1978563cd37d0d832396ef
lvshaokang <lvshaokang@hotmail.com>
2020-12-12 23:39:42 +0800

    feat(617): 合并二叉树,递归

---
+++ 617.合并二叉树.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=java
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    /**
+     * 递归
+     * // 为什么没有写出来
+     * // 思路被限制在了 mergeTrees 的返回结果和新产生的节点的关系
+     */
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return null;
+        }
+
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+
+        TreeNode newNode  = new TreeNode(t1.val + t2.val);
+        newNode.left = mergeTrees(t1.left, t2.left);
+        newNode.right = mergeTrees(t1.right, t2.right);
+        return newNode;
+    }
+}
+// @lc code=end
+
